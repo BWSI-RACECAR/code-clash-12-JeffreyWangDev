e78990b451d86b80c3c5967c8f8e8693de86f80d
Jeffrey Wang <66625372+JeffreyWangDev@users.noreply.github.com>
2023-07-27 09:21:00 -0400

    asd

--- balancechecking.py
+++ balancechecking.py
@@ -28,8 +28,25 @@ Input:{[{}]  Output: False
 """
 class Solution:
     def isBalanced(self, parenthesis): 
-        print(True)
-        print(False)
+        a = 0
+        b = 0
+        c = 0
+        stack = []
+        go = True
+        for i in parenthesis:
+            if i == "[":
+                a+=1
+            elif i == "]":
+                a-=1
+            elif i == "(":
+                b+=1
+            elif i == ")":
+                b-=1
+            elif i == "{":
+                c+=1
+            elif i == "}":
+                c-=1
+        return a == 0 and b == 0 and c == 0
                 
 
     
